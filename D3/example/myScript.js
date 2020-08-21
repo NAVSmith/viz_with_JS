252e00da849fc2af00d60317fc5c8e3fe9d17f3b
NAS <56131958+NAVSmith@users.noreply.github.com>
2020-08-21 12:03:37 +0200

    adding a plotly test notebooks

--- D3/example/myScript.js
+++ D3/example/myScript.js
@@ -2,3 +2,6 @@ d3.select('svg').attr('width', '200px');
 d3.selectAll('p').attr('class', 'info');
 d3.selectAll('h1').style('font', 'Helvetica');
 d3.select('h1').text('My document');
+
+d3.select('body').append('a').attr('href', 'https://d3js.org').text('docs');
+d3.select('table').selectAll('tr').append('td').text('0');
